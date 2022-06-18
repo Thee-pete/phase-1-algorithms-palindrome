cff2477c162ccf9c3b56cefadea689ff35da1e97
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-18 20:09:51 +0300

    commit tree 7794f32ef5575f89dba965876b91f56183941984

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  const charArray = word.split('')
+  for(let i =0; i<= charArray.length-1; i++){
+   
+    if(charArray[i] == charArray[(charArray.length-1) -i]){
+      return true
+    }else{
+      return false
+    }
+  }
+
+
 }
 
-/* 
+/* write a function isPalindrome that takes a string argument
+check if string is palindrome, meaning is characters are the same from start to end:
+
   Add your pseudocode here
 */
 
 /*
+1. convert string to a charcter array
+2. iterate over each character 
+3. check if index i = index length-i 
+4. if the characters match? return true else return false
   Add written explanation of your solution here
 */
 
